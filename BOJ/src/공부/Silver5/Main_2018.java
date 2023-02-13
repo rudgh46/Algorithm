9eebd53a3e8cf2c609e252db482d691c6b0465a3
rudgh46 <rudgh46@naver.com>
2023-02-13 21:25:53 +0900

    수들의 합5

---
+++ BOJ/src/공부/Silver5/Main_2018.java
@@ -0,0 +1,28 @@
+package 공부.Silver5;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_2018 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());	
+		int cnt = 1;
+		int sum = 1;
+		
+		int start = 1;
+		int end = 1;
+		
+		while(end != N) {
+			if(sum == N) {cnt++; end ++; sum += end;}
+			else if(sum > N) {sum -= start; start++;}
+			else {end++; sum += end;}
+		}
+		
+		System.out.println(cnt);
+		
+	}
+
+}
