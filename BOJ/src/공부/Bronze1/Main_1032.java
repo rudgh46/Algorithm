9f5a8238cd5d3c0626840bbe6d8e77fd7febe1e9
rudgh46 <rudgh46@naver.com>
2023-02-23 16:25:41 +0900

    명령 프롬프트

---
+++ BOJ/src/공부/Bronze1/Main_1032.java
@@ -0,0 +1,41 @@
+package 공부.Bronze1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1032 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		String arr[] = new String[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		boolean same = true;
+		
+		for(int i=0; i<arr[0].length(); i++) {
+			char c = arr[0].charAt(i);
+			
+			same = true;
+			for(int j=1; j<N; j++) {
+				if(c != arr[j].charAt(i)) {
+					same=false;
+					break;
+				}
+			}
+			if(same) {
+				sb.append(c);
+			}else
+				sb.append("?");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
