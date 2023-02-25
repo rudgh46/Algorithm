a1490622042dad577856991c45baf09ff2b12d90
rudgh46 <rudgh46@naver.com>
2023-02-25 17:21:54 +0900

    단어 길이 재기

---
+++ BOJ/src/공부/Bronze5/Main_2743.java
@@ -0,0 +1,17 @@
+package 공부.Bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2743 { //단어 길이 재기
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int ans = br.readLine().length();
+		
+		System.out.println(ans);
+	}
+
+}
