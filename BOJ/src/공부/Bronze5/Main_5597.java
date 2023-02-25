ea67db094c45b86dddd7610a044a553d53fa1f6d
rudgh46 <rudgh46@naver.com>
2023-02-25 16:41:41 +0900

    과제안내신분

--- BOJ/src/공부/Bronze5/Main_5597.java
+++ BOJ/src/공부/Bronze5/Main_5597.java
@@ -2,7 +2,7 @@ package 공부.Bronze5;
 
 import java.util.Scanner;
 
-public class Main_5597 {
+public class Main_5597 { //과제 안내신분
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
