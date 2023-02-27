b71412bbd3666f5e6c8b71854d0e771fc2a56a4e
rudgh46 <rudgh46@naver.com>
2023-02-27 23:00:09 +0900

    그대로 출력하기

--- BOJ/src/공부/Bronze5/Main_11718.java
+++ BOJ/src/공부/Bronze5/Main_11718.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_11718 {
+public class Main_11718 { // 그대로 출력하기
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
