4a8fcf38e0c9cb61ba3188ae5612ac2650108cf8
rudgh46 <rudgh46@naver.com>
2023-02-28 14:45:26 +0900

    새싹

---
+++ BOJ/src/공부/Bronze5/Main_25083.java
@@ -0,0 +1,11 @@
+package 공부.Bronze5;
+
+public class Main_25083 { //새싹
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("         ,r'\"7\n").append("r`-_   ,'  ,/\n").append(" \\. \". L_r'\n").append("   `~\\/\n").append("      |\n").append("      |");
+		System.out.println(sb);
+	}
+
+}
