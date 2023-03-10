8495d7bcdf2714dc2c5d3019a5dcd455b2ffc7fc
rudgh46 <rudgh46@naver.com>
2023-03-10 19:04:31 +0900

    좌표 정렬하기2

---
+++ BOJ/src/공부/Silver5/Main_11651.java
@@ -0,0 +1,27 @@
+package 공부.Silver5;
+
+import java.util.*;
+
+public class Main_11651 { //좌표 정렬하기2
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int arr[][] = new int[N][2];
+		
+		for(int i=0; i<N; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr, (o1, o2) ->{
+			if(o1[1] == o2[1]) return o1[0] - o2[0];
+			else return o1[1] - o2[1];
+		});
+		
+		for(int i=0; i<N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+	}
+
+}
