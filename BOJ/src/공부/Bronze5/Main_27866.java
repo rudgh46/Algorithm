7ef6b99cb07156badd5b4427c50f11382fa96cbf
rudgh46 <rudgh46@naver.com>
2023-03-12 19:36:08 +0900

    문자와 문자열

---
+++ BOJ/src/공부/Bronze5/Main_27866.java
@@ -0,0 +1,17 @@
+package 공부.Bronze5;
+
+import java.util.*;
+import java.io.*;
+
+public class Main_27866 { //문자와 문자열
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		System.out.println(str.charAt(Integer.parseInt(br.readLine())-1));
+		
+		
+	}
+
+}
