7cb043ea474df351421efaad176a867e0efedbef
rudgh46 <rudgh46@naver.com>
2023-03-20 23:40:26 +0900

    사파리월드

---
+++ BOJ/src/공부/Bronze5/Main_2420.java
@@ -0,0 +1,15 @@
+package 공부.Bronze5;
+
+import java.util.*;
+
+public class Main_2420 { //사파리월드
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Long N = sc.nextLong();
+		Long M = sc.nextLong();
+		
+		System.out.println(Math.abs(N-M));
+	}
+
+}
